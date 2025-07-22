851dd065679159bc9c9785736f40c0fa0713b956
M.P. Korstanje <rien.korstanje@gmail.com>
2025-07-22 16:11:27 +0200

    Add coverage for uri formatting

---
+++ java/src/main/java/io/cucumber/jsonformatter/RelativeUriFormatter.java
@@ -0,0 +1,28 @@
+package io.cucumber.jsonformatter;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.function.Function;
+
+class RelativeUriFormatter implements Function<URI, URI> {
+    private final URI base;
+
+    public RelativeUriFormatter(URI base) {
+        this.base = base;
+    }
+
+    @Override
+    public URI apply(URI uri) {
+        if (!"file".equals(uri.getScheme())) {
+            return uri;
+        }
+
+        try {
+            URI relative = base.relativize(uri);
+            // Scheme is lost by relativize
+            return new URI("file", relative.getSchemeSpecificPart(), relative.getFragment());
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+    }
+}
