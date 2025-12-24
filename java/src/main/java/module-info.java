bd40e823c2b7812963bff76cfca121382cd1bbdd
M.P. Korstanje <rien.korstanje@gmail.com>
2025-12-24 18:27:35 +0100

    Introduce module

---
+++ java/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.cucumber.jsonformatter {
+    requires org.jspecify;
+    
+    requires io.cucumber.messages;
+    requires io.cucumber.query;
+
+    exports io.cucumber.jsonformatter;
+}
