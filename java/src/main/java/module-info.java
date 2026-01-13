2aa0e5b5154bc6d2249a037edc9026a1852d6c57
M.P. Korstanje <rien.korstanje@gmail.com>
2026-01-13 14:34:37 +0100

    Revert "WIP"
    This reverts commit 48f50daed515083ea0f1b534d588416ae596d89a.

--- java/src/main/java/module-info.java
+++ java/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module io.cucumber.jsonformatter {
     requires org.jspecify;
     
-    requires transitive io.cucumber.messages;
+    requires io.cucumber.messages;
     requires io.cucumber.query;
 
     exports io.cucumber.jsonformatter;
